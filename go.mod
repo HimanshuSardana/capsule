1586aec95b096e17429aeb1cfa545c765542d856
Himanshu Sardana <himanshusardana2005@gmail.com>
2026-04-19 17:28:33 +0530

    feat: add list and run commands with local image registry

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/HimanshuSardana/capsule
 
 go 1.26.2
+
+require golang.org/x/sys v0.43.0
--- main.go
+++ main.go
@@ -1,48 +1,183 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"syscall"
-	_ "time"
 )
+
+type Image struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Images []Image
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "child" {
-		runChild()
+		runChild(os.Args[2])
 		return
 	}
 
-	runParent()
-}
-
-func runParent() {
-	fmt.Println("Parent: Starting container...")
-
-	// Set the necessary flags for creating namespaces
-	cmd := exec.Command("/proc/self/exe", "child")
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS, // New PID, UTS, and Mount namespace
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: capsule <command>")
+		fmt.Println("Commands:")
+		fmt.Println("  list              - List available images")
+		fmt.Println("  run <image> <dir> - Run image in directory")
+		os.Exit(1)
 	}
 
-	// Inherit the standard input/output/error to interact with the child process
+	switch os.Args[1] {
+	case "list":
+		listImages()
+	case "run":
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: capsule run <image> <dir>")
+			os.Exit(1)
+		}
+		runImage(os.Args[2], os.Args[3])
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+}
+
+func listImages() {
+	data, err := os.ReadFile("images.json")
+	if err != nil {
+		fmt.Printf("Error reading images.json: %v\n", err)
+		os.Exit(1)
+	}
+
+	var images Images
+	if err := json.Unmarshal(data, &images); err != nil {
+		fmt.Printf("Error parsing images.json: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, img := range images {
+		fmt.Printf("%s - %s\n", img.Name, img.URL)
+	}
+}
+
+func runImage(imageName, dir string) {
+	img := getImage(imageName)
+	if img.Name == "" {
+		fmt.Printf("Image not found: %s\n", imageName)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Running %s in %s...\n", imageName, dir)
+
+	dir = filepath.Clean(dir)
+	if !filepath.IsAbs(dir) {
+		abs, _ := filepath.Abs(dir)
+		dir = abs
+	}
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		fmt.Printf("Error creating directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	imgPath := filepath.Join("images", imageName+".tar.gz")
+	if _, err := os.Stat(imgPath); err != nil {
+		fmt.Printf("Downloading image...\n")
+		if err := downloadImage(img.URL, imgPath); err != nil {
+			fmt.Printf("Error downloading: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Printf("Extracting image...\n")
+	if err := extractTarGz(imgPath, dir); err != nil {
+		fmt.Printf("Error extracting: %v\n", err)
+		os.Exit(1)
+	}
+
+	runContainer(dir)
+}
+
+func getImage(name string) Image {
+	data, err := os.ReadFile("images.json")
+	if err != nil {
+		return Image{}
+	}
+
+	var images Images
+	if err := json.Unmarshal(data, &images); err != nil {
+		return Image{}
+	}
+
+	for _, img := range images {
+		if img.Name == name {
+			return img
+		}
+	}
+	return Image{}
+}
+
+func downloadImage(url, path string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP %d", resp.StatusCode)
+	}
+
+	os.MkdirAll(filepath.Dir(path), 0755)
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func extractTarGz(tarPath, dest string) error {
+	file, err := os.Open(tarPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	cmd := exec.Command("tar", "-xzf", tarPath, "-C", dest)
+	cmd.Stdin = file
+	return cmd.Run()
+}
+
+func runContainer(rootfs string) {
+	fmt.Println("Parent: Starting container...")
+
+	cmd := exec.Command("/proc/self/exe", "child", rootfs)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS,
+	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// Run the command
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error starting child process: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func runChild() {
+func runChild(rootfs string) {
 	fmt.Println("Child: Inside container (PID namespace)")
 
-	// Chroot into the Alpine rootfs
-	if err := syscall.Chroot("./testenv"); err != nil {
+	if err := syscall.Chroot(rootfs); err != nil {
 		fmt.Printf("Chroot failed: %v\n", err)
 		os.Exit(1)
 	}
@@ -52,24 +187,16 @@ func runChild() {
 		os.Exit(1)
 	}
 
-	// Mount necessary filesystems
 	setupMounts()
 
-	// Explicitly setting the PATH environment variable
 	os.Setenv("PATH", "/bin:/usr/bin:/sbin:/usr/sbin")
 
-	// Try using busybox directly (symlinks break after chroot due to absolute paths)
 	cmd := exec.Command("/bin/busybox", "sh")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error running shell: %v\n", err)
-		fmt.Println("Contents of /bin:")
-		files, _ := os.ReadDir("/bin")
-		for _, f := range files {
-			fmt.Println("  -", f.Name())
-		}
 	}
 }
 
@@ -95,12 +222,4 @@ func setupMounts() {
 	if err := syscall.Mount("devpts", "/dev/pts", "devpts", 0, ""); err != nil {
 		fmt.Printf("Failed to mount /dev/pts: %v\n", err)
 	}
-}
-
-func getHostname() string {
-	hostname, err := os.Hostname()
-	if err != nil {
-		return "unknown"
-	}
-	return hostname
 }
